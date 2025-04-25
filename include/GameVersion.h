ce5d5c91a6bb956c3252d84858b68679bf9c84e0
Vivek.P <vivx_developer@yahoo.in>
2025-04-25 23:44:31 +0530

    updated version

--- include/GameVersion.h
+++ include/GameVersion.h
@@ -2,10 +2,10 @@
 #define GAME_VERSION_H
 
 #define GAME_VERSION_MAJOR 0
-#define GAME_VERSION_MINOR 1
+#define GAME_VERSION_MINOR 2
 #define GAME_VERSION_PATCH 0
 
-#define GAME_VERSION_STRING "0.1.0"
+#define GAME_VERSION_STRING "0.2.0"
 #define GAME_BUILD_DATE __DATE__
 #define GAME_BUILD_TIME __TIME__
 #define GAME_TITTLE "Palette Rain"
